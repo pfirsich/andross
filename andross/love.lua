5beca6595b61806beed3edae7ead452edfedccc7
Joel Schumacher <joelschum@gmail.com>
2017-02-08 16:03:45 +0100

    Introduce love.graphics.multiplyMatrix and bindTransform for attachments

--- andross/love.lua
+++ andross/love.lua
@@ -3,6 +3,16 @@ local class = require "andross.middleclass"
 
 local backend = {}
 
+function love.graphics.multiplyMatrix(matrix)
+    -- we cannot just tell love to multiply a custom matrix to the modelview, so we have to decompose it
+    -- and use the functions love provides
+    love.graphics.translate(matrix[5], matrix[6])
+    -- the 2x2 upper left block of the matrix {a, b, c, d} can be decompsed into a shear and scale
+    -- [s_x, 0, 0, s_y] *  [1, sh_x, sh_y, 1] = [s_x, s_x*sh_x, s_y*sh_y, s_y]
+    love.graphics.scale(matrix[1], matrix[4])
+    love.graphics.shear(matrix[2] / matrix[1], matrix[3] / matrix[4])
+end
+
 backend.ImageAttachment = class("ImageAttachment", andross.Attachment)
 
 function backend.ImageAttachment:initialize(name, image, quad)
@@ -18,15 +28,13 @@ function backend.ImageAttachment:draw(skeleton)
     lg.push()
 
     -- apply bone transform
-    -- we cannot just tell love to multiply a custom matrix to the modelview, so we have to decompose it
-    -- and use the functions love provides
     local bone = skeleton.bones[self.parentBoneName]
-    local bone_wT = bone.worldTransform.matrix
-    lg.translate(bone_wT[5], bone_wT[6])
-    -- the 2x2 upper left block of the matrix {a, b, c, d} can be decompsed into a shear and scale
-    -- [s_x, 0, 0, s_y] *  [1, sh_x, sh_y, 1] = [s_x, s_x*sh_x, s_y*sh_y, s_y]
-    lg.scale(bone_wT[1], bone_wT[4])
-    lg.shear(bone_wT[2] / bone_wT[1], bone_wT[3] / bone_wT[4])
+    lg.multiplyMatrix(bone.worldTransform.matrix)
+
+    lg.push()
+    if self.bindTransform then
+        lg.multiplyMatrix(self.bindTransform.matrix)
+    end
 
     -- draw
     lg.setColor(255, 255, 255, 255)
@@ -35,6 +43,7 @@ function backend.ImageAttachment:draw(skeleton)
     else
         lg.draw(self.image, self.positionX, self.positionY, self.angle, self.scaleX, self.scaleY)
     end
+    lg.pop()
 
     if drawBones then
         lg.rectangle("fill", 0, -20, bone.length, 40)
